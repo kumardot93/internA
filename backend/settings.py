8cf5b79f4366862a2613a9dffee92ac3712d709b
Aditya Kumar <adityakumar@Adityas-MacBook-Air.local>
2021-05-21 21:52:00 +0530

    cors fix

--- backend/settings.py
+++ backend/settings.py
@@ -49,12 +49,12 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
@@ -141,4 +141,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # django_heroku.settings(locals())
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
