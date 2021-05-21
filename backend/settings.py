e92fe61058c5a95261909d14a5ccd4d15c16e80c
Aditya Kumar <adityakumar@Adityas-MacBook-Air.local>
2021-05-21 21:15:18 +0530

    allowed host update

--- backend/settings.py
+++ backend/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-36@%f+tdkr=en=nd1rq-28%szq20ee0^xfk4u6w-n%vy$t1ut^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
